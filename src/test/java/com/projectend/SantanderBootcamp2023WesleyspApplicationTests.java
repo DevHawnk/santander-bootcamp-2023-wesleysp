cbc445b4bf2feeb8aa64fb43c80ad9d75fe90166
DevHawnk <wesley.trabalho42@outlook.com>
2023-09-26 23:38:58 -0300

    Initial commit

---
+++ src/test/java/com/projectend/SantanderBootcamp2023WesleyspApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projectend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SantanderBootcamp2023WesleyspApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
